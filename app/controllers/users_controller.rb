b04c8497949847f113b8f5e03a88d624cfe605b7
Loren Cipriano <loren25cipriano@gmail.com>
2021-04-28 13:21:00 -0700

    created sign up form for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,22 @@
 class UsersController < ApplicationController
-    def index
-        @animes = Anime.all
-      end
-    
-      def new
+
+    def new
         @anime = Anime.new
-      end
-    
-      def create
+    end
+
+    def create 
         @anime = Anime.new(anime_params)
-        if @anime.save 
-          redirect_to anime_path(@anime) 
+        if @anime.save
+            redirect_to anime_path(@anime)
         else 
-          render :new
+            rener :new 
         end 
-      end
+    end     
     
-    private 
+private 
     
-      def anime_params
+    def anime_params
         params.require(:anime).permit(:username, :email, :password)
-      end 
+    end 
     
 end
