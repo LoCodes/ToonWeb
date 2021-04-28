00e3135271e02f0afa90039f5096e25c2771f97c
Loren Cipriano <loren25cipriano@gmail.com>
2021-04-28 15:11:18 -0700

    created user#new view and action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :username, :email, presence: true
-    validates :username, :email, uniqueness: true
-    # validates :email, presence: true 
-    # validates :email, uniqueness: true
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :email, presence: true 
+    validates :email, uniqueness: true
 
     has_many :animes
     has_many :genres, through: :animes
