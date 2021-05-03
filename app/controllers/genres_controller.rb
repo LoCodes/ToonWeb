cc65b5733e78f11fb753eccc157ae2544470cdf6
Loren Cipriano <loren25cipriano@gmail.com>
2021-05-03 12:35:54 -0700

    created genre controller actions and views

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,4 +3,38 @@ class GenresController < ApplicationController
     def index 
         @genres = Genre.all
     end 
+
+    def new 
+        @genre = Genre.new
+    end 
+
+    def create 
+        @genre = Genre.new(genre_params)
+        if @genre.save 
+            redirect_to genre_path(@genre)
+        else
+            render :new 
+        end 
+    end 
+
+    def show 
+        @genre = Genre.find_by_id(params[:id])
+    end 
+
+    # def edit 
+    # end 
+
+    # def update 
+    #     if @genre.update(genre_params)
+    #         redirect_to genre_path(@genre)
+    #     else 
+    #         render :edit 
+    #     end 
+    # end 
+
+private 
+
+    def genre_params
+        params.require(:genre).permit(:name)
+    end 
 end
