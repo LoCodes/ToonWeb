d74e94a354b397505197a3574ef8a22ff7cee955
Loren Cipriano <loren25cipriano@gmail.com>
2021-05-06 13:25:51 -0700

    refactored new view for anime

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -24,18 +24,6 @@ class AnimesController < ApplicationController
   #to render a new form
   #route: animes/new      path/prefix: new_anime_path
   # def new
-  #   @genre = Genre.find_by_id(params[:genre_id])
-
-  #   if params[:genre_id]
-  #     @anime = @genre.animes.build #creates empty associated objects 
-      
-  #   else
-  #     @anime = Anime.new
-  #     # # @anime.genre = @genre 
-  #     # @anime.genres.build 
-  #     # @anime.build_genre #?? double check
-  #   end 
-  # end
 
   def new 
     # THIS IS WHAT YOU WORKING ON !!!!!!
@@ -66,22 +54,7 @@ class AnimesController < ApplicationController
       end
   end 
 
-  # def create  
-  #   # @anime = Anime.new(anime_params)
-  #   # @anime = current_user.animes.build(anime_params)
-  #   if params[:genre_id]
-  #     @genre = Genre.find_by_id(params[:genre_id])
-  #     @anime = @genre.animes.build(anime_params)
-  #   else
-  #     @anime = Anime.new(anime_params)
-  #   end 
 
-  #   if @anime.save 
-  #     redirect_to genre_animes_path(@anime.genre)
-  #   else 
-  #     render :new 
-  #   end 
-  # end 
 
 
   def show
