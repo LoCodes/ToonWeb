2ca290b997dc64be7c4847fcaaf8c9e97f361358
Loren Cipriano <loren25cipriano@gmail.com>
2021-05-09 14:28:44 -0700

    fixed edit/delete link to render and utilized helper method

--- app/helpers/animes_helper.rb
+++ app/helpers/animes_helper.rb
@@ -1,6 +1,17 @@
 module AnimesHelper 
-    
 
+    # can define method send to link else error msg 
+    def edit_link
+        if current_user == @anime.user 
+            link_to "Edit", edit_anime_path(@anime)
+        end 
+
+    end
+    def delete_link 
+        if current_user == @anime.user 
+            link_to "Delete", anime_path(@anime), method: :delete, data: { confirm: 'Are you sure you want to delete this?' }
+        end 
+    end 
 end
 
 
